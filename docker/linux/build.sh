fca7e2f0f4f294300dbd38da9a26ed8c4cb743ce
itsmattkc <34096995+itsmattkc@users.noreply.github.com>
2025-02-18 13:56:39 -0800

    ci: improvements

--- docker/linux/build.sh
+++ docker/linux/build.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
-source /opt/rh/gcc-toolset-14/enable
+
+EXTRA_CMAKE_FLAGS=
+
+if [ "$ARCH" = "aarch64" ]
+then
+    EXTRA_CMAKE_FLAGS="-DCMAKE_CROSSCOMPILING=ON -DCMAKE_SYSTEM_PROCESSOR=aarch64 -DCMAKE_SYSTEM_NAME=Linux -DCMAKE_CROSS_PREFIX=aarch64-linux-gnu-"
+    export PKG_CONFIG_PATH=/usr/lib/aarch64-linux-gnu/pkgconfig
+    export PATH=$PATH:/usr/lib/aarch64-linux-gnu
+    export CC=aarch64-linux-gnu-gcc
+    export AR=aarch64-linux-gnu-ar
+fi
+
 mkdir -p /build
-cmake -S /vanilla -B /build -DVANILLA_BUILD_VENDORED=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo
+cmake -S /vanilla -B /build -DVANILLA_BUILD_VENDORED=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo $EXTRA_CMAKE_FLAGS
 cmake --build /build --parallel
 cmake --install /build --prefix "/install"
