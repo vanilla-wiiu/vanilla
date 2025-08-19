0172f88e43d2d6569fb02482085e41c41da69de6
MattKC <mattkc@noreply.git.mattkc.com>
2025-08-18 17:22:55 -0700

    fix VAAPI on x11

--- gui/ui/ui_sdl.c
+++ gui/ui/ui_sdl.c
@@ -285,14 +285,19 @@ void mic_callback(void *userdata, Uint8 *stream, int len)
 int vui_init_sdl(vui_context_t *ctx, int fullscreen)
 {
 	// Enable Steam Deck gyroscopes even while Steam is open and in gaming mode
-	SDL_SetHintWithPriority("SDL_GAMECONTROLLER_ALLOW_STEAM_VIRTUAL_GAMEPAD", "0", SDL_HINT_OVERRIDE);
-	SDL_SetHintWithPriority(SDL_HINT_GAMECONTROLLER_IGNORE_DEVICES, "", SDL_HINT_OVERRIDE);
+	SDL_SetHint("SDL_GAMECONTROLLER_ALLOW_STEAM_VIRTUAL_GAMEPAD", "0");
+	SDL_SetHint(SDL_HINT_GAMECONTROLLER_IGNORE_DEVICES, "");
 
 	// One-time setup for SDL NV12
-	SDL_SetHintWithPriority("SDL_RENDER_OPENGL_NV12_RG_SHADER", "1", SDL_HINT_OVERRIDE);
+	SDL_SetHint("SDL_RENDER_OPENGL_NV12_RG_SHADER", "1");
 
-	// VAAPI doesn't work with xwayland, so if Wayland is available, use it by default
-	SDL_SetHintWithPriority("SDL_HINT_VIDEODRIVER", "wayland,x11", SDL_HINT_OVERRIDE);
+	// Force EGL when using X11 so that VAAPI works
+	SDL_SetHint(SDL_HINT_VIDEO_X11_FORCE_EGL, "1");
+
+	// By default, SDL2 uses X11, even on Wayland systems. This probably isn't
+	// a big deal, but just in case we want it to prefer Wayland, this is how
+	// we can do it.
+	// SDL_SetHint(SDL_HINT_VIDEODRIVER, "wayland,x11");
 
     // Initialize SDL
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_GAMECONTROLLER) < 0) {
