8ad70e9eb9e22e2ef8eec3fd122c36dd910de6e6
MattKC <mattkc@noreply.git.mattkc.com>
2025-10-02 15:08:29 -0700

    attempt to set up polkit listener

--- gui/pipemgmt.c
+++ gui/pipemgmt.c
@@ -23,6 +23,90 @@ static int pipe_input;
 static int pipe_err;
 static pthread_t pipe_log_thread = 0;
 
+#ifdef VANILLA_POLKIT_AGENT
+#define POLKIT_AGENT_I_KNOW_API_IS_SUBJECT_TO_CHANGE
+#include <polkit/polkit.h>
+#include <polkitagent/polkitagent.h>
+
+typedef struct {
+	PolkitAgentListener parent;
+} VanillaPolkitListener;
+
+typedef struct {
+	PolkitAgentListenerClass parent_class;
+} VanillaPolkitListenerClass;
+
+G_DEFINE_TYPE(VanillaPolkitListener, vanilla_polkit_listener, POLKIT_AGENT_TYPE_LISTENER)
+
+static void on_request(PolkitAgentSession *s, const gchar *prompt, gboolean echo_on, gpointer user_data)
+{
+	vpilog("Request for password auth!");
+	polkit_agent_session_response(s, "cum!");
+}
+
+static void on_show_info(PolkitAgentSession *s, const gchar *text, gpointer user_data)
+{
+	if (text) g_printerr("%s\n", text);
+}
+
+static void on_show_error(PolkitAgentSession *s, const gchar *text, gpointer user_data)
+{
+	if (text) g_printerr("Error: %s\n", text);
+}
+
+static void on_complete(PolkitAgentSession *s, gboolean gained, gpointer data)
+{
+	GTask *task = G_TASK(data);
+	g_task_return_boolean(task, gained);
+	g_object_unref(task);
+}
+
+static void vanilla_polkit_initiate_authentication(PolkitAgentListener  *listener,
+													const gchar          *action_id,
+													const gchar          *message,
+													const gchar          *icon_name,
+													PolkitDetails        *details,
+													const gchar          *cookie,
+													GList                *identities,
+													GCancellable         *cancellable,
+													GAsyncReadyCallback   callback,
+													gpointer              user_data)
+{
+	// Choose the first identity proposed by polkit. Real agents offer a chooser.
+	PolkitIdentity *id = (PolkitIdentity *) identities->data;
+
+	PolkitAgentSession *session = polkit_agent_session_new(id, cookie);
+	g_signal_connect(session, "request",    G_CALLBACK(on_request),   NULL);
+	g_signal_connect(session, "show-info",  G_CALLBACK(on_show_info), NULL);
+	g_signal_connect(session, "show-error", G_CALLBACK(on_show_error),NULL);
+
+	// When session completes, call the async callback with success/failure.
+	g_signal_connect(session, "completed", G_CALLBACK(on_complete), g_task_new(listener, NULL, NULL, NULL));
+
+	// Start PAM conversation
+	polkit_agent_session_initiate(session);
+}
+
+static gboolean vanilla_polkit_initiate_authentication_finish(PolkitAgentListener *listener,
+                                                  GAsyncResult        *res,
+                                                  GError             **error)
+{
+	// We returned via GTask above; propagate boolean result.
+	return g_task_propagate_boolean(G_TASK(res), error);
+}
+
+static void vanilla_polkit_listener_class_init(VanillaPolkitListenerClass *self) {
+	PolkitAgentListenerClass *lklass = POLKIT_AGENT_LISTENER_CLASS(self);
+	lklass->initiate_authentication        = vanilla_polkit_initiate_authentication;
+	lklass->initiate_authentication_finish = vanilla_polkit_initiate_authentication_finish;
+}
+
+static void vanilla_polkit_listener_init(VanillaPolkitListener *self)
+{
+
+}
+#endif
+
 void *vpi_pipe_log_thread(void *data)
 {
     char buf[512];
@@ -75,6 +159,25 @@ int vpi_start_pipe()
         // Pipe is already active
         return VANILLA_SUCCESS;
     }
+
+#ifdef VANILLA_POLKIT_AGENT
+	gpointer polkit_handle = 0;
+	{
+		g_autoptr(PolkitSubject) subject = polkit_unix_session_new_for_process_sync(getpid(), NULL, NULL);
+		g_autoptr(GError) error = NULL;
+
+		VanillaPolkitListener *listener = g_object_new(vanilla_polkit_listener_get_type(), NULL);
+		polkit_handle = polkit_agent_listener_register(
+			POLKIT_AGENT_LISTENER(listener),
+			POLKIT_AGENT_REGISTER_FLAGS_NONE,
+			subject,
+			NULL, NULL, &error
+		);
+		if (!polkit_handle) {
+			g_printerr("Failed to register Polkit listener: %s\n", error->message);
+		}
+	}
+#endif
 
     // Set up pipes so child stdout can be read by the parent process
     int err_pipes[2], in_pipes[2];
@@ -171,6 +274,12 @@ int vpi_start_pipe()
 
         close(err_pipes[1]);
         close(in_pipes[0]);
+
+#ifdef VANILLA_POLKIT_AGENT
+		if (polkit_handle) {
+			polkit_agent_listener_unregister(polkit_handle);
+		}
+#endif
 
         return ret;
     }
