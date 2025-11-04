f667f08be22722ddc46908c4036a05ce53ec7407
itsmattkc <34096995+itsmattkc@users.noreply.github.com>
2025-11-04 00:48:57 -0800

    ci: oops

--- docker/linux/build.sh
+++ docker/linux/build.sh
@@ -4,7 +4,11 @@ EXTRA_CMAKE_FLAGS=
 
 if [ "$ARCH" = "aarch64" ]
 then
-    EXTRA_CMAKE_FLAGS="-DCMAKE_CROSSCOMPILING=ON -DCMAKE_SYSTEM_PROCESSOR=aarch64 -DCMAKE_SYSTEM_NAME=Linux"
+    EXTRA_CMAKE_FLAGS=(
+		-DCMAKE_CROSSCOMPILING=ON
+		-DCMAKE_SYSTEM_PROCESSOR=aarch64
+		-DCMAKE_SYSTEM_NAME=Linux
+	)
     export PKG_CONFIG_PATH=/usr/lib/aarch64-linux-gnu/pkgconfig
     export PATH=$PATH:/usr/lib/aarch64-linux-gnu
     export CC=aarch64-linux-gnu-gcc
