957987b679c867184f42bdac6dc17b9e74e97dcc
itsmattkc <34096995+itsmattkc@users.noreply.github.com>
2025-12-01 03:28:25 -0800

    use local repo for buildroot build instead of retrieving from git

--- docker/raspberrypi0w/build.sh
+++ docker/raspberrypi0w/build.sh
@@ -3,6 +3,7 @@
 set -e
 
 make -C /buildroot O=/build BR2_EXTERNAL=/vanilla/buildroot vanilla_raspberrypi0w_defconfig
+/buildroot/utils/config --set-str BR2_PACKAGE_VANILLA_LOCAL_PATH /vanilla
 make -C /buildroot O=/build
 
 cd /build/images
