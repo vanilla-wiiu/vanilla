8e2f86712aba0e9020430dea9aea0a592375d380
itsmattkc <34096995+itsmattkc@users.noreply.github.com>
2026-01-01 13:29:24 -0800

    ci: missed another thing

--- docker/raspberrypizero2w/build.sh
+++ docker/raspberrypizero2w/build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-make -C /buildroot O=/build BR2_EXTERNAL=/vanilla/buildroot vanilla_raspberrypizero2w_defconfig
+make -C /buildroot O=/build BR2_EXTERNAL=/vanilla/buildroot vanilla_raspberrypizero2w_64_defconfig
 /buildroot/utils/config --file /build/.config -e BR2_PACKAGE_VANILLA_USE_LOCAL
 /buildroot/utils/config --file /build/.config --set-str BR2_PACKAGE_VANILLA_LOCAL_PATH /vanilla
 make -C /buildroot O=/build
