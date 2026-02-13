b6a1ed8296f1211095a0e0febc8d00873c2cd499
BlocksumoGuys <148206420+BlocksumoGuys@users.noreply.github.com>
2026-02-13 10:01:59 +0100

    lang.c update
    Co-authored-by: VoxelTek <voxeltek@poto.cafe>

--- gui/lang.c
+++ gui/lang.c
@@ -5,13 +5,17 @@
 
 static const char *lang_str[__VPI_LANG_T_COUNT];
 
-void lang_choice(char *curr_lang){
+int lang_choice(char *curr_lang){
     if (strcmp(curr_lang, "en") == 0) {
         memcpy(lang_str, lang_str_en, sizeof(lang_str));
     }
     else if (strcmp(curr_lang, "fr") == 0) {
         memcpy(lang_str, lang_str_fr, sizeof(lang_str));
     }
+    else {
+        return 0;
+    }
+    return 1;
 }
 
 
