815d89032aeb80508b3b16a82155ad0521b9e390
BlocksumoGuys <148206420+BlocksumoGuys@users.noreply.github.com>
2026-02-14 11:18:32 +0100

    lang.c upd
    Added language switching functionality and history tracking.

--- gui/lang.c
+++ gui/lang.c
@@ -2,8 +2,13 @@
 #include "lang.h"
 #include "langs/english_en/english_en.h"
 #include "langs/french_fr/french_fr.h"
-
+static char *langs_order[2] = {
+    "en",
+    "fr",
+};
+static const int lang_c = sizeof(langs_order) / sizeof(langs_order[0]);
 static const char *lang_str[__VPI_LANG_T_COUNT];
+char lang_history[3];
 
 int lang_choice(char *curr_lang){
     if (strcmp(curr_lang, "en") == 0) {
@@ -15,9 +20,24 @@ int lang_choice(char *curr_lang){
     else {
         return 0;
     }
+    memcpy(lang_history, curr_lang, sizeof(lang_history));
     return 1;
 }
 
+char *switch_lang(const char *curr_lang){
+    for(int i = 0; i < lang_c; i++){
+        if(strcmp(langs_order[i], curr_lang) == 0) {
+            int next = (i + 1) % lang_c;
+            lang_choice(langs_order[next]);
+            return langs_order[next];
+        }
+    }
+    return NULL;
+}
+
+const char *get_lang(){
+    return lang_history;
+}
 
 const char *lang(vpi_lang_t id)
 {
