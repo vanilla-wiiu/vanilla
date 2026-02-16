0519c7b1516526cee40f002a4e4c015eebe8b3a1
itsmattkc <34096995+itsmattkc@users.noreply.github.com>
2026-02-15 23:47:26 -0800

    docker: skip android signing if no key present

--- docker/android/build.sh
+++ docker/android/build.sh
@@ -3,4 +3,9 @@ set -e
 cd /vanilla/android
 ./gradlew -PbuildDir=/build assembleRelease
 zipalign -v -p 4 /build/outputs/apk/release/app-release-unsigned.apk /build/outputs/apk/release/app-release-unsigned-aligned.apk
-apksigner sign --ks /vanilla/docker/android/vanilla.jks --out /install/app-release.apk --ks-pass pass:$ANDROID_SIGNING_KEY /build/outputs/apk/release/app-release-unsigned-aligned.apk
+
+# If signing key is available, sign the APK
+if [[ -n "$ANDROID_SIGNING_KEY" ]]
+then
+    apksigner sign --ks /vanilla/docker/android/vanilla.jks --out /install/app-release.apk --ks-pass pass:$ANDROID_SIGNING_KEY /build/outputs/apk/release/app-release-unsigned-aligned.apk
+fi
