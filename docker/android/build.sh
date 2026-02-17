45a2dd236365d767aa9b3a9c512f9f5abadab157
itsmattkc <34096995+itsmattkc@users.noreply.github.com>
2026-02-16 17:55:39 -0800

    docker: on unsigned android builds, copy the apk into place

--- docker/android/build.sh
+++ docker/android/build.sh
@@ -8,4 +8,7 @@ zipalign -v -p 4 /build/outputs/apk/release/app-release-unsigned.apk /build/outp
 if [[ -n "$ANDROID_SIGNING_KEY" ]]
 then
     apksigner sign --ks /vanilla/docker/android/vanilla.jks --out /install/app-release.apk --ks-pass pass:$ANDROID_SIGNING_KEY /build/outputs/apk/release/app-release-unsigned-aligned.apk
+else
+    # Otherwise, copy into the location expected by other scripts
+    cp /build/outputs/apk/release/app-release-unsigned-aligned.apk /install/app-release.apk
 fi
